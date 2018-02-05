b2eedfa40dd7f77f60b851196c124dab2f0f7ca4
SungminPark1 <sp4216@rit.edu>
2018-02-05 10:26:41 -0500

    arrows now ignore collisions with objects on layer 9: Traps (#7)

--- One Plus/Assets/Scripts/ArrowScript.cs
+++ One Plus/Assets/Scripts/ArrowScript.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class ArrowScript: MonoBehaviour {
 	public float speed = 1;
-	public float rotationDegree = 0;
-//	Collider2D player = GetComponent(Collider2D);
 
+	private Rigidbody2D rigidbody;
 	// Use this for initialization
 	void Start () {
-		Vector3 startRotation = new Vector3 (0, 0, rotationDegree);
 
-		transform.Rotate (startRotation);
+		// ignore collisions with othe traps
+		Physics2D.IgnoreLayerCollision (9, 9, true);
 	}
 	
 	// Update is called once per frame
@@ -24,6 +23,8 @@ public class ArrowScript: MonoBehaviour {
 		// destroy self it collision with ground
 		if (collision.gameObject.tag == "Ground") {
 			Destroy (this.gameObject);
+		} else if (collision.gameObject.tag == "Player") {
+			Destroy(this.gameObject);
 		}
 	}
 }
