2101ac7f02508f22bab2f990b6bfea5b4bd4a8fd
Corinne Green (RIT Student) <cmg7063@ad.rit.edu>
2018-02-15 10:33:47 -0500

    fix squirrel animation, idle state

--- One Plus/Assets/Scripts/SquirrelScript.cs
+++ One Plus/Assets/Scripts/SquirrelScript.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class SquirrelScript : MonoBehaviour {
 	public bool inGround = true;
 
-	public GameObject throwingObject;
+    private Animator animator;
+
+    public GameObject throwingObject;
 	public float throwSpeed = 1;
 	public Vector3 ThrowDirection = Vector3.left;
 
@@ -14,33 +16,42 @@ public class SquirrelScript : MonoBehaviour {
 	private float timeLeft;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		timeLeft = timeOffset;
-	}
+        animator = GetComponent<Animator>();
+    }
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update ()
+    {
 		timeLeft -= Time.deltaTime;
 
 		// fires an arrow and restarts the timer
-		if (timeLeft <= 0) {
+		if (timeLeft <= 0)
+        {
 			FireObject ();
-
-			timeLeft = 5;
+            animator.SetTrigger("throw");
+            timeLeft = 5;
 		}
 	}
 
 	// fires the throwing object
-	public void FireObject() {
+	public void FireObject()
+    {
 		GameObject Clone;
+        animator.SetTrigger("throw");
 
-		//spawning the object at position
-		Clone = (Instantiate(throwingObject, transform.position, transform.rotation)) as GameObject;
+        //spawning the object at position
+        Clone = (Instantiate(throwingObject, transform.position, transform.rotation)) as GameObject;
 
 		// if inGround spawn with larger offset (prevent object instantly destorying itself by ground collision)
-		if (inGround) {
+		if (inGround)
+        {
 			Clone.transform.Translate (ThrowDirection * 0.70f);
-		} else {
+		}
+        else
+        {
 			Clone.transform.Translate (ThrowDirection * 0.20f);
 		}
 
